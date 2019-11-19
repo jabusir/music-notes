c2241ec89bdd3d15b26bac9e92f6e6a2776ee6f1
Jejo Abusir <jabusir@gmail.com>
2019-11-18 16:06:16 -0800

    End of data sanitization and validatoin

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -9,50 +9,68 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     email: {
         type: String,
         required: true,
+        trim: true,
+        lowercase: true,
         validate(value) {
-            if (!validate.isEmail(value)) {
+            if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid')
             }
         }
     },
     age : {
         type: Number,
+        default: 0,
         validate(value) {
             if (value < 0) {
                 throw new Error('Age must be a positive number')
             }
         }
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot include "password"')
+            }
+        }
     }
 })
 
-// const me = new User({
-//     name: 'Jejo',
-//     age: 21
-// })
+const me = new User({
+    name: '    Jejo   ',
+    email: 'Jaabusir@gmail.com  ',
+    password: 'bitchnibba'
+})
 
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((err) => {
-//     console.log('error: ', err)
-// })
+me.save().then(() => {
+    console.log(me)
+}).catch((err) => {
+    console.log('error: ', err)
+})
 
 const Task = mongoose.model('Task', {
     desc: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 const Hw = new Task({
-    desc: 'Do my hw',
-    completed: false
+    desc: 'Do my hw      ',
 })
 
 Hw.save().then(() => {
