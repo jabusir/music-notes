30d69dd23a07777a78619effbab18fcfdb51940a
jabusir <jabusir@gmail.com>
2020-06-08 22:51:01 -0700

    Implement friend request routes

--- src/routers/friend.js
+++ src/routers/friend.js
@@ -7,9 +7,10 @@ router.post('/friends/add/:id', async (req, res) => {
     const _id = req.params.id
     const senderId = req.body.senderId
     try {
-        let user = await User.findOneAndUpdate({ _id }, { $push: { friendRequestsRecieved: [senderId] } })
-        const sender = await User.findByIdAndUpdate({ _id: senderId }, { $push: { friendRequestsSent: [_id] } })
-        user.save()
+        let user = await User.findByIdAndUpdate({ _id }, { $push: { friendRequestsRecieved: [senderId] } })
+        let sender = await User.findByIdAndUpdate({ _id: senderId }, { $push: { friendRequestsSent: [_id] } })
+        await sender.save()
+        await user.save()
         res.status(201).send({ user })
     } catch (e) {
         res.status(400).send(e)
@@ -17,5 +18,36 @@ router.post('/friends/add/:id', async (req, res) => {
 
 })
 
+router.post('/friends/accept', async (req, res) => {
+    const { senderId, _id } = req.body
+    try {
+        let user = await User.findByIdAndUpdate({ _id }, { $pullAll: { friendRequestsRecieved: [senderId] }, $push: { friends: [senderId] } })
+        let sender = await User.findByIdAndUpdate({ _id: senderId }, { $pullAll: { friendRequestsSent: [_id] }, $push: { friends: [_id] } })
+        await user.save()
+        await sender.save()
+
+        console.log(user, sender)
+        //remove user from requsted list of sender
+        res.status(201).send({ user })
+    } catch (e) {
+        res.status(500).send(e)
+    }
+
+})
+
+router.post('/friends/decline', async (req, res) => {
+    const { senderId, _id } = req.body
+    try {
+        let user = await User.findByIdAndUpdate({ _id }, { $pullAll: { friendRequestsRecieved: [senderId] } })
+        let sender = await User.findByIdAndUpdate({ _id: senderId }, { $pullAll: { friendRequestsSent: [_id] } })
+        await sender.save()
+        await user.save()
+        res.status(201).send({ user })
+        //remove user from requsted list of sender
+    } catch (e) {
+        res.status(400).send(e)
+    }
+
+})
 
 module.exports = router
